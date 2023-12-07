c8740b773f80c8969396b14e92d8af0ffebf8329
prodgers1 <pcrodgers1@gmail.com>
2023-12-07 03:13:46 +0000

    Day 4

--- Day04/Day04.py
+++ Day04/Day04.py
@@ -5,3 +5,31 @@ from collections import defaultdict
 sys.path.append('./')
 from adventInput import GetInput
 _input = GetInput(4)
+
+ans = 0
+scratchers = defaultdict(int)
+for line in _input:
+  card, rest = line.split(":")
+  cardNum = int(card.split()[1])
+  winningNumbers, myNumbers = rest.strip().split('|')
+  winningNumbers = winningNumbers.strip()
+  myNumbers = myNumbers.strip()
+  
+  winningSet = set(winningNumbers.split())
+  myNumbersSet = set(myNumbers.split())
+
+  scratchers[cardNum] += 1
+
+  overlap = myNumbersSet.intersection(winningSet)
+  exponent = len(overlap) - 1
+
+  for i in range(1, len(overlap) + 1):
+    scratchers[cardNum+i] += (1 * scratchers[cardNum]) 
+
+  if len(overlap) == 0:
+    continue
+  
+  ans += int((2**exponent))
+
+print(ans)
+print(sum([v for k, v in scratchers.items()]))
